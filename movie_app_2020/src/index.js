4c006fa9fbdc8dbbcee40720368c86590ca5bd19
kangeunhyeon <rkddmsgus1@daum.net>
2021-02-02 20:53:47 +0900

    movieapp

---
+++ movie_app_2020/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById('tomato'));//react application 는 하나의 컴포넌트만 렌더링 할수 있음
+
+
