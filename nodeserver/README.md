d6b663d0de541396ea431004ba4d101f7c34d73d
kangeunhyeon <rkddmsgus1@daum.net>
2021-02-02 20:54:46 +0900

    nodeserver

---
+++ nodeserver/README.md
@@ -0,0 +1,1 @@
+#Node.js 웹개발로 알아보는 백엔드 Javascript
---
+++ nodeserver/router/main/main.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express()
+var path = require('path');
+var router = express.Router();
+
+router.get('/', function(req,res){
+    console.log('main js loaded')
+	res.sendFile(path.join(__dirname, '../public/main.html'))
+});
+
+module.exports = router;
